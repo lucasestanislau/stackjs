ef084ff42e807d252a627fff525f6dacc6cf8be7
Lucas Vidoto <lucasvidoto17x@gmail.com>
2020-04-10 15:09:57 -0300

    mobile da app

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+//video 4 - tempo 48:34
